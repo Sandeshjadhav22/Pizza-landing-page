3aabd31bef249c489818eec711d6eb961ab67d1a
sandy <sandeshjadhav2002@gmail.com>
2024-04-23 20:43:40 +0530

    lorem changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello  sandyy</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, animi.</p>
       <Button >Button</Button>
 
     </main>
