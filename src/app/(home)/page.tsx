144ef77b0e3e78a57eeee63e0f72c8f35ee0564d
sandy <sandeshjadhav2002@gmail.com>
2024-04-23 22:56:53 +0530

    add logo and select box

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Hello  sandyy</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, animi.</p>
       <Button >Button</Button>
 
-    </main>
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/custom/header";
 
-const manrope = Manrope({ subsets: ["latin"],variable: "--font-manrope", });
+const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
 export const metadata: Metadata = {
   title: "Pizza Shop",
@@ -17,10 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
+      <body
+        className={cn(
           "min-h-screen bg-background font-manrope antialiased",
           manrope.variable
-        )}>{children}</body>
+        )}
+      >
+     <Header/>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
