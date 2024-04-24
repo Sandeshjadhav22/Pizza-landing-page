256061c8d1503feb511faeb0b5527c4ca03b5891
sandy <sandeshjadhav2002@gmail.com>
2024-04-24 14:51:59 +0530

    create topping checkbox group

--- src/app/(home)/components/topping-list.tsx
+++ src/app/(home)/components/topping-list.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+'use client'
+import React, { useState } from "react";
+import ToppingCard, { Topping } from "./topping-cart";
+
+const toppings = [
+  {
+    id: "1",
+    name: "Chicken",
+    image: "/chicken.png",
+    price: 50,
+    isAvailable: true,
+  },
+  {
+    id: "2",
+    name: "jelapeno",
+    image: "/Jelapeno.png",
+    price: 50,
+    isAvailable: true,
+  },
+  {
+    id: "3",
+    name: "Cheese",
+    image: "/cheese.png",
+    price: 50,
+    isAvailable: true,
+  },
+];
 
 const ToppingList = () => {
-  return (
-    <div>ToppingList</div>
-  )
-}
+  const [selectedToppings, setSelectedToppings] = React.useState([toppings[0]]);
 
-export default ToppingList
+  const handleCheckBoxCheck = (topping: Topping) => {
+    const isAlreadyExists = selectedToppings.some(
+      (element) => element.id === topping.id
+    );
+    if (isAlreadyExists) {
+      setSelectedToppings((prev) =>
+        prev.filter((elm) => elm.id !== topping.id)
+      );
+      return;
+    }
+
+    setSelectedToppings((prev) => [...prev,topping])
+  };
+  return (
+    <section className="mt-6">
+      <h3>Extra topping</h3>
+      <div className="grid grid-cols-3 gap-4 mt-2">
+        {toppings.map((topping) => {
+          return (
+            <ToppingCard
+              topping={topping}
+              key={topping.id}
+              selectedToppings={selectedToppings}
+              handleCheckBoxCheck={handleCheckBoxCheck}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ToppingList;
